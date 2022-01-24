f6983e7694616b5fedf2d24ca2f86db72df43590
Dickson <hhhh820605@gmail.com>
2022-01-24 21:24:24 +0800

    create Respondent

--- user/models.py
+++ user/models.py
@@ -7,5 +7,19 @@ class City(models.Model):
     name = models.CharField(max_length=50, unique=True, null=False)
     createdon = models.DateField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
+
+
+class Respondent(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
     def __str__(self):
         return self.name
